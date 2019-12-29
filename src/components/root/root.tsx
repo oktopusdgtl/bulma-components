837a69536428cbac86a347c0c7d8dc3cfe2dc55a
mespiritu <marcespiritu17@gmail.com>
2019-12-29 10:06:20 +0800

    fix in documentaion url

--- src/components/root/root.tsx
+++ src/components/root/root.tsx
@@ -15,18 +15,18 @@ export class Root {
               <okt-column columns="three-quarters">
                 <stencil-router>
                   <stencil-route-switch scrollTopOffset={0}>
-                    <stencil-route url="/" component="okt-demo" exact />
-                    <stencil-route url="/button" component="okt-demo-button" exact />
-                    <stencil-route url="/column" component="okt-demo-column" exact />
+                    <stencil-route url="/bulma-components/" component="okt-demo" exact />
+                    <stencil-route url="/bulma-components/button" component="okt-demo-button" exact />
+                    <stencil-route url="/bulma-components/column" component="okt-demo-column" exact />
                   </stencil-route-switch>
                 </stencil-router>
               </okt-column>
 
               <okt-column>
                 <div class="nav">
-                  <okt-button inverted color="link" tag="a" href="/" fullWidth>Home</okt-button>
-                  <okt-button inverted color="link" tag="a" href="/button" fullWidth>Button</okt-button>
-                  <okt-button inverted color="link" tag="a" href="/column" fullWidth>Column</okt-button>
+                  <okt-button inverted color="link" tag="a" href="/bulma-components/" fullWidth>Home</okt-button>
+                  <okt-button inverted color="link" tag="a" href="/bulma-components/button" fullWidth>Button</okt-button>
+                  <okt-button inverted color="link" tag="a" href="/bulma-components/column" fullWidth>Column</okt-button>
                 </div>
               </okt-column>
             </okt-columns>
